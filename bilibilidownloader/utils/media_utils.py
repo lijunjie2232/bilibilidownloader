ccfcf1cbdde5837c4f46e1f478912834ec2f88fe
lijunjie2232 <2533584225@qq.com>
2025-08-11 19:10:27 +0900

    utils

---
+++ bilibilidownloader/utils/media_utils.py
@@ -0,0 +1,56 @@
+_QN_240P = 6
+_QN_360P = 16
+_QN_480P = 32
+_QN_720P = 64
+_QN_720P_60 = 74
+_QN_1080P = 80
+
+_ACODEC_64K = 30216
+_ACODEC_132K = 30232
+_ACODEC_192K = 30280
+_ACODEC_DB = 30250
+_ACODEC_HR = 30251
+
+_CODEC_AVC = 7
+_CODEC_HEVC = 12
+_CODEC_AV1 = 13
+
+_VQ = {
+    6: "240P",
+    16: "360P",
+    32: "480P",
+    64: "720P",
+    80: "1080P",
+}
+
+_AQ = {
+    30216: "64K",
+    30232: "132K",
+    30280: "192K",
+    30250: "DB",
+    30251: "HR",
+}
+
+_CODECID = {
+    7: "AVC",
+    12: "HEVC",
+    13: "AV1",
+}
+
+
+def get_vq(video_quality):
+    if video_quality in _VQ:
+        return _VQ[video_quality]
+    return None
+
+
+def get_aq(audio_quality):
+    if audio_quality in _AQ:
+        return _AQ[audio_quality]
+    return None
+
+
+def get_codec(codec_id):
+    if codec_id in _CODECID:
+        return _CODECID[codec_id]
+    return None
