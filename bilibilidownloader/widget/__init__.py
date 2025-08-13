1acda4cfbb1a8122ea08ea2b3c6c3233c9941e56
lijunjie2232 <2533584225@qq.com>
2025-08-13 10:17:02 +0900

    widget init

---
+++ bilibilidownloader/widget/__init__.py
@@ -0,0 +1,14 @@
+from .AnalyzerWidget import AnalyzerWidget, DownloadCommit
+from .DownloadWidget import DownloadTask, DownloadTaskWidget, TaskState
+from .LoginDialog import LoginDialog
+from .TaskManager import TaskManager
+
+__all___ = [
+    "DownloadTaskWidget",
+    "DownloadTask",
+    "TaskState",
+    "TaskManager",
+    "AnalyzerWidget",
+    "DownloadCommit",
+    "LoginDialog",
+]
