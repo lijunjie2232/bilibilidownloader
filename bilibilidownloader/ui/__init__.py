41331381202ace1bbb189b832603a1ab46fddb22
lijunjie2232 <2533584225@qq.com>
2025-08-13 10:16:47 +0900

    ui

---
+++ bilibilidownloader/ui/__init__.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+from .analyzer_task_ui import Ui_AnalyzerTask
+from .analyzer_ui import Ui_Analyzer
+from .download_task_ui import Ui_DownloadTask
+from .login_dialog_ui import Ui_LoginDialog
+from .main_ui import Ui_MainWindow
+
+__MODULE_PATH__ = Path(__file__).parent.resolve()
+
+__all__ = [
+    "__MODULE_PATH__",
+    "Ui_AnalyzerTask",
+    "Ui_Analyzer",
+    "Ui_DownloadTask",
+    "Ui_MainWindow",
+    "Ui_LoginDialog",
+]
