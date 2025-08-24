a65b6715a0a9f0e299c3fce52f97f26933adf11f
lijunjie2232 <2533584225@qq.com>
2025-08-24 11:17:54 +0900

    icon

--- icon_rc.py
+++ icon_rc.py
@@ -112,17 +112,10 @@ qt_resource_struct = b"\
 \x00\x00\x01\x98\x01\xad\xcc@\
 "
 
-
 def qInitResources():
-    QtCore.qRegisterResourceData(
-        0x03, qt_resource_struct, qt_resource_name, qt_resource_data
-    )
-
+    QtCore.qRegisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
 
 def qCleanupResources():
-    QtCore.qUnregisterResourceData(
-        0x03, qt_resource_struct, qt_resource_name, qt_resource_data
-    )
-
+    QtCore.qUnregisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
 
 qInitResources()
